a1c5b924d276a3484fffb8f84820b1a88ff4d2f6
Leone Dantas <leonedantas80@gmail.com>
2023-08-01 15:43:59 -0300

    c

--- src/features/filter/FilterInterface.tsx
+++ src/features/filter/FilterInterface.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useContext, useEffect } from "react";
 import { Form } from "react-router-dom";
 import { AppContext } from "../../pages/Root";
